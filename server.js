6e2f2f13e269580d248905fd21660de2bc6cd76e
Boitemogelo Steenkamp <yandisa867@gmail.com>
2026-03-05 00:11:22 +0200

    Use PORT env var for hosting

--- server.js
+++ server.js
@@ -125,3 +125,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
+
+app.get("/", (req, res) => {
+  res.send("Unboxd API is running");
+});
